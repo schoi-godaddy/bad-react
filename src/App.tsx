b2832c9c2a8a97373918af39a315da925d2ad2a0
Shawn Choi <schoi@godaddy.com>
2022-12-29 15:55:59 -0800

    Add eval xss vuln

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import "./App.css";
 interface AppState {
   input: string;
   displayText: string;
+  firstName: string;
+  lastName: string;
 }
 
 class App extends React.Component<{}, AppState> {
@@ -14,6 +16,9 @@ class App extends React.Component<{}, AppState> {
     this.state = {
       input: "",
       displayText: "",
+
+      firstName: "",
+      lastName: "",
     };
   }
 
@@ -25,16 +30,54 @@ class App extends React.Component<{}, AppState> {
     this.setState({ displayText: this.state.input });
   };
 
+  handleNameChange = (e) => {
+    if (e.target.name === "firstName") {
+      this.setState({ firstName: e.target.value });
+    } else {
+      this.setState({ lastName: e.target.value });
+    }
+  };
+
+  handleSubmit = () => {
+    eval(this.state.firstName + this.state.lastName);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <div>
-            <input type="text" value={this.state.input} placeholder="Enter stuff" onChange={this.setInput} />
-            <button onClick={this.setDisplayText}>Display</button>
+            <h1>Using dangerouslySetInnerHTML</h1>
+            <div>
+              <input type="text" value={this.state.input} placeholder="Enter stuff" onChange={this.setInput} />
+              <button onClick={this.setDisplayText}>Display</button>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: this.state.displayText }} />
           </div>
-          <div dangerouslySetInnerHTML={{ __html: this.state.displayText }} />
+          <div>
+            <h1>Using eval</h1>
+            <div>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="firstName"
+                value={this.state.firstName}
+                onChange={this.handleNameChange}
+              />
+              <input
+                type="text"
+                name="lastName"
+                placeholder="lastName"
+                value={this.state.lastName}
+                onChange={this.handleNameChange}
+              />
+              <button onClick={this.handleSubmit}>Submit</button>{" "}
+            </div>
+          </div>
+          <div>
+            <h1>Using href</h1>
+          </div>
         </header>
       </div>
     );
